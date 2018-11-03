ae5fd9c60cc38c0b096650f757d716b7af170320
Alberto Martinez <jose_alberto12216@hotmail.com>
2018-11-03 12:33:57 -0600

    nuevas versiones

--- routes/documents.js
+++ routes/documents.js
@@ -4,14 +4,22 @@ const Document = require('../models/Document');
 const upload = require('../helpers/multer');
 
 router.post('/createDocument', upload.single('name') ,(req,res) => {
-  const {repository,description} = req.body;
+  const {repository,description,parentDocument} = req.body;
   var locationUrl = req.file.url,
       name= req.file.originalname;
-  console.log(`repository: ${repository}, name: ${name}, description: ${description}, url: ${locationUrl} `);
-  Document.create({name,description,repository,locationUrl})
+  console.log(`repository: ${repository}, name: ${name}, description: ${description}, url: ${locationUrl}, parentDocument: ${parentDocument} `);
+  if(parentDocument === "undefined") {
+    Document.create({name,description,repository,locationUrl})
     .then(() => {
       res.redirect(`/repositories/${repository}`);
     })
+  }else{
+    Document.create({name,description,repository,locationUrl,parentDocument})
+    .then(() => {
+      res.redirect(`/repositories/${repository}`);
+    })
+  }
+  
 });
 
 module.exports = router;
